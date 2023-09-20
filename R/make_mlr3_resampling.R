cf00d31ef228db196b95994d086e4151f5e3855c
nt-williams <ntwilliams.personal@gmail.com>
2023-09-20 12:03:52 -0700

    Bug fix when creating CV folds with grouping variable

--- R/make_mlr3_resampling.R
+++ R/make_mlr3_resampling.R
@@ -4,8 +4,11 @@ make_mlr3_resampling <- function(task, folds) {
     return(out)
   }
 
+  if (is.null(task$groups)) clusters <- NULL
+  else clusters <- task$data(cols = task$groups)[, 1]
+
   ofolds <- origami::make_folds(task$nrow,
-                                cluster_ids = task$groups,
+                                cluster_ids = clusters,
                                 strata_ids = task$data(cols = task$target_names)[, 1])
   out <- rsmp("custom")
   out$instantiate(task,
