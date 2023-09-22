5839d8d977e5699a101fb977b1b82e5f5d6fa877
nt-williams <ntwilliams.personal@gmail.com>
2023-09-22 12:03:34 -0700

    Changing loss function depending on outcome type

--- R/compute_super_learner_weights.R
+++ R/compute_super_learner_weights.R
@@ -6,7 +6,7 @@ compute_super_learner_weights <- function(learners, y, outcome_type) {
               })
   x <- matrix(Reduce(`c`, x), ncol = length(learners))
   ids <- unlist(lapply(learners, function(x) x$learner$id))
-  cvRisk <- apply(x, 2, function(X) mean((X - y)^2))
+  cvRisk <- apply(x, 2, function(X) compute_loss(X, y, outcome_type))
   names(cvRisk) <- ids
   colnames(x) <- ids
   task <- make_mlr3_task(data.frame(x, y), "y", "continuous")
@@ -19,3 +19,12 @@ compute_super_learner_weights <- function(learners, y, outcome_type) {
   metalearner$train(task)
   list(risk = cvRisk, metalearner = metalearner)
 }
+
+compute_loss <- function(x, y, outcome_type) {
+  switch(outcome_type,
+         binomial = loss_nll(x, y),
+         continuous = loss_mse(x, y))
+}
+
+loss_mse <- function(x, y) mean((x - y)^2)
+loss_nll <- function(x, y) -mean(y*log(x))
